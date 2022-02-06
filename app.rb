584607e6c5b3b8a66a78082ff40de70241b9add1
sheridan <sheridanmaustad@gmail.com>
2022-02-06 11:55:17 -0700

    commit tree e6c63c4bcd2cf8d8c9f9594644f0cc23376df93a

---
+++ app.rb
@@ -0,0 +1,22 @@
+# puts "Hello World!"
+# puts "Hello World!"
+# puts "Hello World!"
+
+# print ("Hello World!")
+# print ("Hello World!")
+# print ("Hello World!")
+
+# puts [1, 2, 3].inspect
+
+# p [1, 2, 3]
+
+# pp [{ id: 1, hello: "World" }, { id: 2, hello: "Ruby" }, { id: 3, hello: "Moon" }, { id: 4, hello: "Learner" }]
+
+# lines 9 and 11 are doing the same thing
+# both line 1 and 5 do the same thing
+
+puts "Hello World!"
+
+print "Pass this test, please."
+
+p [1, 2, 3]
